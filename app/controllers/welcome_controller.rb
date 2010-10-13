541840a894f2af3f230d42432bb84a3697ec13d2
Andrei Fokau <andrei.fokau@neutron.kth.se>
2010-10-13 15:52:23 +0200

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+    @feed = 'RSS feed'
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Highflux::Application.config.secret_token = 'a72d1dc70d31aef9725324ca68721bb80fcd0c1477bd8d6693341835fae2627d5a0b50ea60c2fb0b971311ad4c3313246b8c5b6a6f05543dda2494c0c5051721'
